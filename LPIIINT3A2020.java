2ac29231719d3fa4997cbb5bd97dc561c58c6ab0
Ldeandradeforte-br <60586310+Ldeandradeforte-br@users.noreply.github.com>
2020-02-12 14:06:46 -0400

    Add files via upload

---
+++ LPIIINT3A2020.java
@@ -0,0 +1,11 @@
+package lpiiint3a2020;
+
+//@author Aluno Lucas De Andrade Forte
+
+public class LPIIINT3A2020 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
