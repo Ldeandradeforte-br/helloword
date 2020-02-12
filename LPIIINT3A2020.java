fb4063a96e69562fadf4a1dd99355a92aea301f0
Ldeandradeforte-br <60586310+Ldeandradeforte-br@users.noreply.github.com>
2020-02-12 15:05:29 -0400

    Add files via upload
    aula 12/02/2020

--- LPIIINT3A2020.java
+++ LPIIINT3A2020.java
@@ -1,11 +1,21 @@
 package lpiiint3a2020;
 
 //@author Aluno Lucas De Andrade Forte
-
+import javax.swing.JOptionPane;
 public class LPIIINT3A2020 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        String firstNumber =
+                JOptionPane.showInputDialog( "Enter first integer");
+        String secondNumber =
+                JOptionPane.showInputDialog( "Enter second integer");
+        
+        int number1 = Integer.parseInt( firstNumber );
+        int number2 = Integer.parseInt( secondNumber );
+        
+        int sum = number1 + number2;
+        
+        JOptionPane.showMessageDialog(null, "the sum is" + sum,
+                "Sum of Two Integers", JOptionPane.PLAIN_MESSAGE);
     }
-    
 }
